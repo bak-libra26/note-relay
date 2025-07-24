ab2d3aa8212c1f98df91406e23bbbaa386bc477c
092600 <bak.libra26@gmail.com>
2025-07-24 17:59:49 +0900

    .

--- main.ts
+++ main.ts
@@ -1,86 +1,44 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, Notice } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface AutoPushSyncSettings {
+	serverUrl: string;
+	syncPassword: string; // apiKey → syncPassword로 변경
+	autoSync: boolean;
+	syncInterval: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: AutoPushSyncSettings = {
+	serverUrl: '',
+	syncPassword: '',
+	autoSync: true,
+	syncInterval: 10,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+export default class AutoPushSyncPlugin extends Plugin {
+	settings: AutoPushSyncSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new AutoPushSyncSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// 플러그인 활성화 시 동기화 타이머 등록
+		if (this.settings.autoSync) {
+			this.registerInterval(
+				window.setInterval(() => {
+					this.syncWithServer();
+				}, this.settings.syncInterval * 60 * 1000)
+			);
+		}
 	}
 
-	onunload() {
-
+	async syncWithServer() {
+		new Notice('동기화 테스트: syncWithServer()가 실행되었습니다!');
+		// 실제 동기화 로직 대신 알림만 띄움
 	}
+
+
+	onunload() {}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,44 +49,53 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AutoPushSyncSettingTab extends PluginSettingTab {
+	plugin: AutoPushSyncPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AutoPushSyncPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Server URL')
+			.setDesc('Enter the URL of your remote sync backend server.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('https://your-server.com/api')
+				.setValue(this.plugin.settings.serverUrl)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.serverUrl = value;
 					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Sync Password')
+			.setDesc('Enter a password to authenticate with your sync server. This acts as your secret key for secure synchronization.')
+			.addText(text => text
+				.setPlaceholder('Enter your sync password')
+				.setValue(this.plugin.settings.syncPassword)
+				.onChange(async (value) => {
+					this.plugin.settings.syncPassword = value;
+					await this.plugin.saveSettings();
+				}));
+
+
+		new Setting(containerEl)
+			.setName('Sync Interval (minutes)')
+			.setDesc('Set how often to sync automatically (in minutes).')
+			.addText(text => text
+				.setPlaceholder('10')
+				.setValue(this.plugin.settings.syncInterval.toString())
+				.onChange(async (value) => {
+					const num = Number(value);
+					if (!isNaN(num) && num > 0) {
+						this.plugin.settings.syncInterval = num;
+						await this.plugin.saveSettings();
+					}
 				}));
 	}
 }
